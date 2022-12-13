0a31391a453caa02e6783925abc35688cd16c7fa
Dino Cajic <dinocajic@gmail.com>
2022-12-13 15:31:24 -0500

    Laravel Vue Vite

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="p-4">Welcome Page from Vue</h1>
+    <div class="p-4">
+        <a href="/">Home</a> | <a href="/about">About</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+    h1, div {
+        background-color: cornflowerblue;
+        color: white;
+    }
+</style>
